08f5acb3047c71464f4036efd5e0cbfc859f5dcc
Surendra <surendrababubhupathi@gmail.com>
2024-05-30 16:16:10 +0530

    add data

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Arithmetic_Operators{
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number : ");
+        a = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        b = sc.nextInt();
+        c = a + b;
+        System.out.println("The Addition of "+a+" and "+b+" are "+ (a+b));
+        System.out.println("The Subtraction of "+a+" and "+b+" are "+ (a-b));
+        System.out.println("The Multiplication of "+a+" and "+b+" are "+ (a*b));
+        System.out.println("The Division of "+a+" and "+b+" are "+ (a/b));
+        System.out.println("The Modulo of "+a+" and "+b+" are "+ (a%b));
+    }
+}
