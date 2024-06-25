79df2c461936b7fc61b9007019d36afaa30f8017
Surendra Babu <surendrababubhupathi@gmail.com>
2024-06-25 15:10:03 +0530

    Commit

---
+++ ArrayList1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+
+        //Initialize ArrayList without Elements and fixed datatype as String
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("Suri");
+        arr.add("Shiva");
+
+        System.out.println(arr);
+
+    }
+}
